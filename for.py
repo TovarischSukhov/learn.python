20516b9dcd180e41c41163b29c7a07cfb7bfd3ae
Zhohal Dmitry <50736689+dzhohal@users.noreply.github.com>
2019-05-23 13:57:59 +0300

    Задача по for

--- for.py
+++ for.py
@@ -10,12 +10,24 @@
 * Посчитать и вывести средний балл по каждому классу.
 """
 
-def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
-    
+def main(ocenki):
+  vseshkola = 0
+  vesklass = 0
+  kol = 0
+  vsekol = 0
+  p = ""
+  for klass in ocenki:
+    for bal in klass["scores"]:
+        vesklass += bal
+        vseshkola += bal
+        kol += 1
+        vsekol += 1
+    p += "Средний балл "+ klass["school_class"] + " класса:" + str(vesklass/kol)+ "\n" 
+    vesklass = 0
+    kol = 0
+  return(print(p + "Средний балл школы: " + str(vseshkola/vsekol)))
 if __name__ == "__main__":
-    main()
+    ocenki = [{'school_class': '4a', 'scores': [3,4,4,5,2]},
+    {'school_class': '3b', 'scores': [2,4,5,5,5]},
+    {'school_class': '2c', 'scores': [2,2,3,3,3]}]
+main(ocenki)
